661c9d02fb2287969bbd64f402b59d040c433ba7
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-11-23 15:20:21 -0400

    create auth definitions

---
+++ Todo.Api/Common/Auth/Providers/Policies.cs
@@ -0,0 +1,6 @@
+namespace Todo.Api.Common.Auth.Providers;
+
+public static class Policies
+{
+    public const string Global = nameof(Global);
+}
---
+++ Todo.Api/Common/Auth/Providers/Schemes.cs
@@ -0,0 +1,6 @@
+namespace Todo.Api.Common.Auth.Providers;
+
+public static class Schemes
+{
+    public const string Default = nameof(Default);
+}
