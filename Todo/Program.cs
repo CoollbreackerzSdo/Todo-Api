fc65b000e53b09311562b13f7caa6067ffa88b1b
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-11-23 17:00:43 -0400

    fixed comments error

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
 
 builder.Services.AddHttpClient("server", options =>
 {
