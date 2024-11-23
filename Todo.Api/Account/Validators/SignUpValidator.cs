b516aa0ac4260bc6ca3e9080a38ac0a37c39b2b4
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-11-23 16:31:07 -0400

    update validator minimum size

--- Todo.Api/Account/Validators/SignUpValidator.cs
+++ Todo.Api/Account/Validators/SignUpValidator.cs
@@ -12,7 +12,7 @@ public sealed class SignUpValidator : AbstractValidator<SignUpRequest>
             .MaximumLength(50);
 
         RuleFor(x => x.Password)
-            .MaximumLength(8)
+            .MinimumLength(8)
             .MustAsync((x, _) => Task.FromResult(x.Count(char.IsNumber) >= 4)).WithMessage("The Password property must be at least more than 4 numeric characters")
             .MustAsync((x, _) => Task.FromResult(x.Count(char.IsUpper) >= 2)).WithMessage("The Password property must be at least more than 2 uppercase characters");
     }
