aec9d1eaaa669529f8d03c34905d8acc9c4fa7ae
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-11-25 17:32:29 -0400

    update tailwind

--- Todo/tailwind.config.js
+++ Todo/tailwind.config.js
@@ -1,9 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./Components/**/*.{razor,html}', '../Todo.Client/**/*.{razor,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
-
