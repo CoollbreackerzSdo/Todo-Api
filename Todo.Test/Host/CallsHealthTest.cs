fbd929bbc9001b7ac2d70ae7e40ff0f7f61c57f4
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-11-25 21:57:04 -0400

    update host tests

---
+++ Todo.Test/Host/CallsHealthTest.cs
@@ -0,0 +1,44 @@
+using Aspire.Hosting;
+
+namespace Todo.Test.Host;
+
+public class CallsHealthTest : IAsyncLifetime
+{
+    [Fact]
+    public async Task GetApiHealthStatusReturnsOkStatusCode()
+    {
+        // Arrange
+        var resourceNotificationService = _app.Services.GetRequiredService<ResourceNotificationService>();
+        // Act
+        var httpClient = _app!.CreateHttpClient("api");
+        await resourceNotificationService.WaitForResourceAsync("api", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/health");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    [Fact]
+    public async Task GetWebHealthStatusReturnsOkStatusCode()
+    {
+        // Arrange
+        var resourceNotificationService = _app.Services.GetRequiredService<ResourceNotificationService>();
+        // Act
+        var httpClient = _app!.CreateHttpClient("client");
+        await resourceNotificationService!.WaitForResourceAsync("client", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/health");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    public async Task DisposeAsync() => await _app!.DisposeAsync();
+    public async Task InitializeAsync()
+    {
+        var appHost = await DistributedApplicationTestingBuilder
+            .CreateAsync<Projects.Todo_AppHost>();
+
+        _app = await appHost.BuildAsync();
+
+        await _app!.StartAsync();
+    }
+    private DistributedApplication _app = null!;
+}
