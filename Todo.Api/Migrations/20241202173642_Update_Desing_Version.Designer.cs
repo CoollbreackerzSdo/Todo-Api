a813e3cc2418ec2df2c7e7060269391381d19148
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-12-02 13:37:52 -0400

    update packs

--- Todo.Api/Migrations/20241202173642_Update_Desing_Version.Designer.cs
+++ Todo.Api/Migrations/20241202173642_Update_Desing_Version.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Todo.Api.Account.Context;
@@ -11,9 +12,11 @@ using Todo.Api.Account.Context;
 namespace Todo.Api.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20241202173642_Update_Desing_Version")]
+    partial class Update_Desing_Version
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
